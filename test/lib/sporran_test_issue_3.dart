ba10147909379cc34a91ce9504309d940febc3e2
steve <steve.hamblett@linux.com>
2014-09-04 12:57:17 +0100

    Issue 3 test harness - delete when issue resolved

---
+++ test/lib/sporran_test_issue_3.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:sporran/sporran.dart';
+import 'package:json_object/json_object.dart';
+import 'sporran_test_config.dart';
+import 'package:unittest/unittest.dart';
+
+main() {
+
+  Sporran sporran;
+
+  String docIdPutOnline = "putOnlineg3";
+  String docIdPutOffline = "putOfflineg3";
+  JsonObject onlineDoc = new JsonObject();
+  JsonObject offlineDoc = new JsonObject();
+  String onlineDocRev;
+
+  var wrapper1 = () {
+
+    if (sporran.lawnIsOpen) {
+
+      var docwrapper = (res) {
+
+        expect(res.ok, isTrue);
+        expect(res.operation, Sporran.PUT);
+        expect(res.localResponse, isFalse);
+        expect(res.id, docIdPutOnline);
+        expect(res.rev, anything);
+        onlineDocRev = res.rev;
+        expect(res.payload.name, "Online");
+
+      };
+
+      onlineDoc.name = "Online";
+      sporran.put(docIdPutOnline, onlineDoc)..then((res) {
+            docwrapper(res);
+          });
+
+    }
+
+  };
+
+  var wrapper = () {
+
+    sporran.online = true;
+    Timer timer = new Timer(new Duration(seconds: 3), wrapper1);
+
+  };
+
+
+  sporran = new Sporran(databaseName, hostName, true, port, scheme, userName, userPassword);
+  sporran.autoSync = false;
+  sporran.onReady.first.then((e) => wrapper());
+
+
+}
